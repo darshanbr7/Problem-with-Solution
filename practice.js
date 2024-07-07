94437c6e8921647d9d545c8a1a11a962152c6e8c
darshanbr7 <darshanbr770@gmail.com>
2024-07-07 19:34:35 +0530

    added

--- practice.js
+++ practice.js
@@ -1,9 +1,12 @@
-//  no of accurence
-function findSumEelement (str){
-    let obj = {}
-     for(let ele of str){
-        (obj[ele]) ? obj[ele]++ : obj[ele] = 1
-     }
-     console.log( obj);
-}
-findSumEelement("hello")
+/* let user1 = { name : "darshan", age : 21};
+let user2 = user1;
+user2.age = 22
+console.log(user1); */
+
+let user3 = {name : "raju", age : 47}
+let user4 = Object.assign({},user3);
+let user5 = Object.assign(user3);
+user4.place = "bolare";
+user4.age = 48;
+console.log(user3);
+console.log(user4);
